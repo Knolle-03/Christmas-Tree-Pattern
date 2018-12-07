c699fa5aaaf793bf60ef259b073a0670298b2e5a
RGnehr <r.gnehr@gmx.de>
2018-12-07 16:10:28 +0100

    first half of order 2

--- array_tree.rb
+++ array_tree.rb
@@ -3,27 +3,27 @@
 # Author:: Lennart Draeger
 # Author:: Robert Gnehr
 
-require_relative 'tree_node'
-
 # Class description
 class ArrayTree
   def initialize(n)
-    raise ArgumentError, "Argument must be Integer." unless n.is_a?(Integer)
+    raise ArgumentError, "Argument must be Integer greater than 0." unless n.is_a?(Integer) && n > 0
 
-    @tree = nil
+    @tree = [%w[0 1]]
     grow_new_tree(n)
   end
 
+  def show_tree
+    # put tree here
+    @tree.to_s
+  end
+
+  def tree
+    @tree.join("\n")
+  end
+
+  private
+
   def grow_new_tree(n)
-    raise ArgumentError, "Argument must be Integer." unless n.is_a?(Integer)
-
-    if n < 1
-      puts 'Gardening warning: No new tree grown.'
-      return
-    else
-      @tree = [[0, 1]]
-    end
-
     (n-1).times do
       grow_one_order
       # TODO: endlosschleife. moep.
@@ -32,35 +32,32 @@ class ArrayTree
   end
 
   def grow_one_order
+    skip_next = false
     @tree.each_with_index do |row, index|
-      grow_row(row, index)
+      next if skip_next
+      skip_next = grow_row(row, index)
     end
   end
 
   def grow_row(row, index)
+    skip_next = false
       # copy first element and insert (shortened) prior line
-    row.insert(1, row[0])
-    @tree.insert(index, row.drop(2)) unless row.drop(2).empty?
-      # add 0s in first line and 0,0,1,1,1... in second line
-    @tree[index].each { |v| v << 0 }
-    row.each_with_index { |v, i| v << i < 1 ? 0 : 1 }
-  end
-
-  def show_tree
-    if @tree == nil
-      puts "Alas, there was no tree. Maybe grow one."
-    else
-
+    row.insert(1, row[0].dup)
+    if row.size > 2
+      tmp = []
+      row.each_with_index { |element, i| tmp << element.dup if i > 1 }
+      @tree.insert(index, tmp)
+      # @tree.insert(index, row.drop(2).dup)
+      skip_next = true
     end
-  end
+      # add 0s in first line and 0,1,1... in second line
+    @tree[index].each { |element| element << '0' }
+    row.each_with_index { |element, i| element << ( i < 1 ? '0' : '1' ) }
 
-  def tree
-    @tree.dup
+    skip_next
   end
 end
 
-tree = ArrayTree.new(12)
+tree = ArrayTree.new(3)
 puts tree.tree
 tree.show_tree
-tree.grow_new_tree(2)
-tree.grow_new_tree(0)
